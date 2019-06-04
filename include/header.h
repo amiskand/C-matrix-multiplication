e58f629d5de59be9874565f52cd9e60cb7dee06c
amiskand <amiskand@ncsu.edu>
2019-06-04 03:01:45 -0400

    created a include folder for the header files

---
+++ include/header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+
+int timing(bool var);
+//char[][] sparseMtrxMult();
+char* mtrxMult(char a,char b, char i, char k, char j);
+
+#endif
