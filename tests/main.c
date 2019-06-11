7d0663b8a387327c59165bb009d7c1e0eebb0ad2
amiskand <amiskand@ncsu.edu>
2019-06-11 19:59:16 -0400

    two steps back one step forward

--- tests/main.c
+++ tests/main.c
@@ -28,73 +28,62 @@ int main(){
   char b[n][p];   //second matrix
   short c[m][p];  //result matrix
 
+
   //loop variables i,k,j
   //they unsigned variables because we don't want negative indices
   unsigned char i = 0;  //row index for matrix a and c
   unsigned char k = 0;  //another index for matrix a and b
   unsigned char j = 0;  //column index for matrix b and c
-
-//  c[0][0]=0; //makes sure first summation is always 0
   
   char start, end;  //timing variables used for computations
 
-  srand(time(0));
+  //clear all data in matrices a and b
+  for(k=0; k<n; k++){
 
-  start = timing(0);  //start timing
+    for(i=0; i<m; i++){
 
-  //computation
-
-  for(i=0;i<m;i++){
-    for(j=0;j<p;j++){
-      for(k=0;k<n;k++){
-
-    
-        a[i][k] = (rand()%127);
-
-//printf(" %d  \n", a[i][k]);
-
-        printf("a(%d,%d):%d ", i,k,a[i][k]);  //display first matrix
-
-//        printf("a:%d", a[i][k]);
-
-        b[k][j] = (rand()%127);
-
-//printf(" %d  \n", b[k][j]);
-
-        printf("b(%d,%d):%d ", k,j,b[k][j]);  //display second matrix
-
-//        printf("b:%d" , b[k][j]);
-
-//multiply then add the result to the previous result     
-         c[i][j] += a[i][k] * b[k][j];
-
-//printf(" %d  \n", c[i][j]);
-
-
-        printf("c(%d,%d):%d      \n", i,j,c[i][j]);  //display results
-//        printf("c:%d   ", c[i][j]);
-
-      }
-
-
-//should c be added here?
-
-
-//        printf("c:%d   ", c[i][j]);
-//    printf("\n");
+      a[i][k] = 0;
 
     }
 
-//  printf("\n");
+    for(j=0;j<p;j++){ 
+
+      b[k][j]=0;
+
+    }
 
   }
 
 
+  //inputs random variables 0-126 in matrices a and b
+  for(k=0; k<n; k++){
+
+    for(i=0; i<m; i++){
+ 
+      a[i][k] = rand()%127;
+
+      printf("a(%d,%d):%d \n", i,k,a[i][k]);  //display first matrix
+ 
+    }
+
+    for(j=0;j<p;j++){ 
+
+      b[k][j] = rand()%127;
+
+      printf("b(%d,%d):%d \n", k,j,b[k][j]);  //display second matrix
+
+    }
+
+  }
+
+  start = timing(0);  //start timing
+
+  //compute
 
   end = timing(1);   //end timing
 
   printf("time it took %d s\n", end - start);  //displaying length of time the program took to run
-
+  
 
   return 0;
 }
